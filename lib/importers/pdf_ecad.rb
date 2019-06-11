3268f472cdf8213dd0a5ccacbd065aa7ea52708e
rfukui <ricardo.fukui@resultadosdigitais.com.br>
2019-06-11 20:04:12 -0300

    refactoring

--- lib/importers/pdf_ecad.rb
+++ lib/importers/pdf_ecad.rb
@@ -1,9 +1,9 @@
 require "pdf-reader"
 
 class Importers::PdfEcad
-  attr_accessor :text
   CATEGORIES = {"CA" => "Author", "E" => "Publisher", "V" => "Versionist", "SE" => "SubPublisher"}
   @line = ""
+
    def initialize(path)
      file = PDF::Reader.new(path)
      @text = []
@@ -64,59 +64,61 @@ class Importers::PdfEcad
    end
    private
 
-   def ipi
-     ipi_ = clean_string(@line.length - 65," ").gsub(".","")
-     return ipi_ if ipi_.length > 0
-
-   end
-
-   def society_name
-     clean_string @line.length - 52
-   end
-
-   def share
-    @line.slice(110..115).strip.gsub(",",".").to_f
-   end
-
-   def role
-    CATEGORIES[@line.slice(106..110).strip]
+   def id
+     @line.slice(0..11).strip
    end
 
    def iswc
      @line.slice(12..28).strip
    end
 
-   def title
-     clean_string 34
+   def ipi
+     ipi_ = clean_string(@line.length - 65," ").gsub(".","")
+     return ipi_ if ipi_.length > 0
+
+   end
+
+    def role
+     CATEGORIES[@line.slice(106..110).strip]
+    end
+
+   def share
+    @line.slice(110..115).strip.gsub(",",".").to_f
    end
 
    def name
      clean_string 12
    end
 
+   def title
+     clean_string 34
+   end
+
    def pseudo
      clean_string 50
+   end
+
+   def society_name
+     clean_string @line.length - 52
    end
 
    def situation
      @line.slice(90..110).strip
    end
 
-   def holder?
-     @line.length>123 and @line =~ /^[0-9]/ and not @line =~ /^[0-9][0-9]\/[0-9][0-9]/
-   end
-
    def created_at
      @line.slice(@line.length-11..).strip
    end
 
-   def id
-     @line.slice(0..11).strip
-   end
 
    def clean_string(position, pattern="  ")
      @line.slice(position..(position + 1 + @line.slice(position + 2..).index(pattern))).strip
    end
+
+   def holder?
+     @line.length>123 and @line =~ /^[0-9]/ and not @line =~ /^[0-9][0-9]\/[0-9][0-9]/
+   end
+
    def work?
       @line =~ /[0-9][0-9]\/[0-9][0-9]\/[0-9][0-9][0-9][0-9]\n/
    end
